ff95bf54789949ca8459acf2f821ceb8d787017e
Emison Dias <56531290+Vinn4k@users.noreply.github.com>
2021-06-23 21:00:11 -0300

    teste erro banco de dados

--- src/main/java/com/example/demo/domain/NewsDb.java
+++ src/main/java/com/example/demo/domain/NewsDb.java
@@ -4,12 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+
 @Entity
 public class NewsDb {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
+	@Lob
 	private String noticia;
 	
 	public Integer getId() {
